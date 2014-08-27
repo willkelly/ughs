096e8382d2aa251a586c2dff1a6e3deeb977f17e
William Kelly <the.william.kelly@gmail.com>
2014-08-27 08:36:21 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="ughs",
+      version="0.1",
+      description="User Group Homogenization System",
+      author="William Kelly",
+      author_email="the.william.kelly@gmail.com",
+      packages=["ughs"],
+      install_requires=["flask>=0.10"]
+)
